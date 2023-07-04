f71d962542964a819b4ce35e8e0f8b1bfc03b977
Damir <damir24may@mail.ru>
2023-07-04 03:14:13 +0300

    added outlet component

--- src/App.js
+++ src/App.js
@@ -8,25 +8,21 @@ import { NotFound } from "./pages/NotFound";
 import { Cart } from "./pages/Cart";
 import "./scss/app.scss";
 import { SinglePizza } from "./pages/SinglePizza";
+import { MainLayout } from "./layouts/MainLayout";
 // export const SearchContext = createContext();
 
 function App() {
   // const [searchValue, setSearchValue] = useState("");
 
   return (
-    <div className="wrapper">
-      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}> */}
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<SinglePizza />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-      {/* </SearchContext.Provider> */}
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<SinglePizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
