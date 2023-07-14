866c261eaee54cb6430ccfe873cb6f75cf03df52
Damir <damir24may@mail.ru>
2023-07-14 15:18:38 +0300

    added code splitting with react-loadable

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,32 @@
-import { lazy, Suspense } from "react";
+import Loadable from "react-loadable";
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import "./scss/app.scss";
 import { MainLayout } from "./layouts/MainLayout";
 
-const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
-const SinglePizza = lazy(
-  () => import(/* webpackChunkName: "SinglePizza" */ "./pages/SinglePizza")
-);
-const NotFound = lazy(
-  () => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound")
-);
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: "Cart" */ "./pages/Cart"),
+  loading: () => <div>Идет загрузка корзины....</div>,
+});
+const SinglePizza = Loadable({
+  loader: () =>
+    import(/* webpackChunkName: "SinglePizza" */ "./pages/SinglePizza"),
+  loading: () => <div>Идет загрузка....</div>,
+});
+const NotFound = Loadable({
+  loader: () => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound"),
+  loading: () => <div>Идет загрузка....</div>,
+});
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route
-          path="cart"
-          element={
-            <Suspense fallback={<div>Идет загрузка корзины....</div>}>
-              <Cart />
-            </Suspense>
-          }
-        />
-        <Route
-          path="pizza/:id"
-          element={
-            <Suspense fallback={<div>Идет загрузка корзины....</div>}>
-              <SinglePizza />
-            </Suspense>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<div>Идет загрузка корзины....</div>}>
-              <NotFound />
-            </Suspense>
-          }
-        />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<SinglePizza />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
